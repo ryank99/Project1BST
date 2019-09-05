d3b557fb3cbf3ca2d5bc0fbaac2fc1106b810c32
ryank99 <38142837+ryank99@users.noreply.github.com>
2019-09-05 15:05:02 -0400

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,39 @@
+package Proj1BST;
+
+public class Name implements Comparable{
+    private String first;
+    private String last;
+    
+    public Name(String f, String l) {
+        first = f.toLowerCase();
+        last = l.toLowerCase();
+    }
+    
+    public String getFirst() {
+        return first;
+    }
+    
+    public String getLast() {
+        return last;
+    }
+    
+    public String toString() {
+        return first+' '+last;
+    }
+    //if this is less than n, return -1
+    //if equal, return 0
+    //if this is bigger, return 1
+    @Override
+    public int compareTo(Object n) {
+        if (n instanceof Name) {
+            if (this.last.compareTo( ((Name)n).getLast() ) == 0) {
+                return -1*this.first.compareTo( ((Name)n).getFirst());
+            }
+            else 
+                return -1*this.last.compareTo( ((Name)n).last);
+        }
+        return 99;
+    }
+
+  
+}
