8f377b2d8d0dcdc893a7757b5cf9f88d45db29b9
ryank99 <38142837+ryank99@users.noreply.github.com>
2019-09-17 14:10:55 -0400

    Add files via upload
    finished dumpsection(), remove(), insert(), section()

--- CourseManager.java
+++ CourseManager.java
@@ -4,28 +4,28 @@ public class CourseManager {
 
     private Section[] sections;
     private int currSection = 1;
-    
+    //finished(probably)
     public CourseManager() {
         sections = new Section[3];
         for(int i = 0; i < sections.length; i++) {
             sections[i] = new Section(i+1);
         }
     }
-    
+    //finished(probably)
     public void section(int n) {
         System.out.println("section changed from "+currSection+" to " + n);
         currSection = n;
     }
     
+    //finished(probably)
     public void insert(Name n) {
         sections[currSection-1].getRoster().insert( new Student(n, generateID()) );
-        System.out.println(sections[currSection-1].getRoster().toString());
     }
-    
+    //finished(probably)
     public void remove(Name n) {
-        sections[currSection].getRoster().remove(n);
+        sections[currSection-1].getRoster().remove(new Student(n, ""));
     }
-    
+    //finished(probably)
     public void removeSection(int n) {
         sections[n-1] = new Section(n);
     }
@@ -37,9 +37,9 @@ public class CourseManager {
     public void score() {
         
     }
-    
+    //finished(probably)
     public String dumpsection() {
-       return(sections[currSection-1].toString());
+       return("Section "+currSection+" dump:\n" + sections[currSection-1].toString());
         //inorder
     }
     
