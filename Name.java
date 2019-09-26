7f9559afc1a7cffa0619e8f7c69f6e0f153be478
ryank99 <38142837+ryank99@users.noreply.github.com>
2019-09-26 12:17:04 -0400

    Add files via upload

--- Name.java
+++ Name.java
@@ -1,36 +1,59 @@
 
-
-public class Name implements Comparable{
+/**
+ * 
+ * @author Ryan Kirkpatrick
+ * @version 1
+ */
+public class Name implements Comparable {
     private String first;
     private String last;
     
+    /**
+     * 
+     * @param f is first
+     * @param l is last
+     */
     public Name(String f, String l) {
         first = f.toLowerCase();
         last = l.toLowerCase();
     }
     
+    /**
+     * 
+     * @return first name
+     */
     public String getFirst() {
         return first;
     }
     
+    /**
+     * 
+     * @return last name
+     */
     public String getLast() {
         return last;
     }
     
+    /**
+     * @return string representation of name
+     */
     public String toString() {
-        return first+' '+last;
+        return first + ' ' + last;
     }
-    //if this is less than n, return -1
-    //if equal, return 0
-    //if this is bigger, return 1
-    @Override
+
+    /**
+     * @override
+     * @param n object to compare
+     * @return the int. >1 if bigger, <1 less, 0 if equal. 
+     */
     public int compareTo(Object n) {
         if (n instanceof Name) {
             if (this.last.compareTo( ((Name)n).getLast() ) == 0) {
                 return this.first.compareTo( ((Name)n).getFirst());
             }
-            else 
+            else {
                 return this.last.compareTo( ((Name)n).last);
+            }
         }
         return 99;
     }
--- Student.java
+++ Student.java
@@ -1,5 +1,9 @@
 
-
+/**
+ * 
+ * @author Ryan Kirkpatrick
+ * @version 1
+ */
 public class Student implements Comparable{
     private Name name;
     private int score;
@@ -7,6 +11,11 @@ public class Student implements Comparable{
     private String id;
     private int level;
     
+    /**
+     * 
+     * @param n name
+     * @param i id
+     */
     public Student(Name n, String i) {
         name = n;
         score = 0;
@@ -15,45 +24,82 @@ public class Student implements Comparable{
         level = 0;
     }
     
+    /**
+     * 
+     * @return this
+     */
     public Student getThis() {
         return this;
     }
+    
+    /**
+     * 
+     * @param s newscore
+     */
     public void setScore(int s) {
         score = s;
     }
     
+    /**
+     * 
+     * @param g newgrade
+     */
     public void setGrade(String g) {
         grade = g;
     }
     
+    /**
+     * 
+     * @return name this.name
+     */
     public Name getName() {
         return name;
     }
     
+    /**
+     * 
+     * @param id newid
+     */
     public void setID(String id) {
         this.id = id;
     }
     
+    /**
+     * 
+     * @return currgrade
+     */
     public String getGrade() {
         return grade;
     }
     
+    /**
+     * 
+     * @return currscore
+     */
     public int getScore() {
         return score;
     }
     
-
-    
+    /**
+     * 
+     * @return currid 
+     */
     public String getID() {
         return id;
     }
 
-
-    @Override
+    /**
+     * @Override
+     * @param arg0 object to compare to
+     * @return encapsulated compareTo
+     */
     public int compareTo(Object arg0) {
         return this.name.compareTo( ((Student)arg0).getName() );
     }
-    //id, name, score, level
+    
+    /**
+     * @return id, name, score=score
+     */
     public String toString() {
         return id+ ", "+ name.toString() + ", "+"score = "+score;
     }
